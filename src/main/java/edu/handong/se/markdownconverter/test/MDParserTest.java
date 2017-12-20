423ed308952c80aa96417c8759cbb9bc5e28f3d8
hecban <21300653@handong.edu>
2017-12-20 23:39:24 +0900

    JUnit second working

--- src/main/java/edu/handong/se/markdownconverter/test/MDParserTest.java
+++ src/main/java/edu/handong/se/markdownconverter/test/MDParserTest.java
@@ -66,7 +66,7 @@ public class MDParserTest
 		html3.setType("?");
 		assertEquals("Thank you", html2.getId());
 		assertEquals("Are", html1.getType());
-		assertEquals("You", html1.getLink());
+		assertEquals("http://You", html1.getLink());
 		assertEquals("?", html1.getTitle());
 	}
 
@@ -99,50 +99,35 @@ public class MDParserTest
 	@Test
 	public void inputTest() {
 		MarkdownConverter MC = new MarkdownConverter();
-		String inputArgs1[] = {"-i", "a.md", "b.md", "-o", "a.html", "b.html"};
+		String inputArgs1[] = {"--input", "a.md", "b.md", "--output", "a.html", "b.html"};
 		MC.main(inputArgs1);
 
-		String inputArgs2[] = {"-h"};
+		String inputArgs2[] = {"--h"};
 		MC.main(inputArgs2);
 
 		String inputArgs3[] = {"-t"};
 		MC.main(inputArgs3);
 
-		String inputArgs4[] = {"-i", "a.md", "-o", "a.html", "b.html"};
+		String inputArgs4[] = {"--input", "a.md", "--output", "a.html", "b.html"};
 		MC.main(inputArgs4);
 
-		String inputArgs5[] = {"-o", "a.html", "b.html"};
+		String inputArgs5[] = {"--output", "a.html", "b.html"};
 		MC.main(inputArgs5);
 
 		String inputArgs6[] = {""};
 		MC.main(inputArgs6);
 
-		String inputArgs7[] = {"-i", "a.md", "b.md"};
+		String inputArgs7[] = {"--input", "a.md", "b.md"};
 		MC.main(inputArgs7);
 
 	}
 
 	@Test
 	public void fileTest() {
-		String fileName1 = "sample/doc1.md";
-		Document doc1 = new Document(fileName1, "doc1.html");
+		String fileName1 = "sample/sampledoc.md";
+		Document doc1 = new Document(fileName1, "sampledoc.html");
 		MDParser MD1 = new MDParser(doc1);
 		MD1.parse();
-
-		String fileName2 = "sample/doc2.md";
-		Document doc2 = new Document(fileName2, "doc2.html");
-		MDParser MD2 = new MDParser(doc2);
-		MD2.parse();
-
-		String fileName3 = "sample/doc3.md";
-		Document doc3 = new Document(fileName3, "doc3.html");
-		MDParser MD3 = new MDParser(doc3);
-		MD3.parse();
-
-		String fileName4 = "sample/doc4.md";
-		Document doc4 = new Document(fileName4, "doc4.html");
-		MDParser MD4 = new MDParser(doc4);
-		MD4.parse();
 	}
 
 
@@ -153,43 +138,6 @@ public class MDParserTest
 		String fileName1 = "sample/doc1.md";
 		Document doc1 = new Document(fileName1, "doc1.html");
 		pv.visitDocument(doc1);
-	/*
-		QuotedBlock qb = new QuotedBlock();
-		Text t = new Text("AAA");
-		qb.addText(t);
-		pv.visitStructure(qb);
-
-		Header hd = new Header("ext", 2);
-		hd.addText(t);
-		pv.visitStructure(hd);
-
-		Block b = new Block();
-		b.addText(t);
-
-		ItemList il = new ItemList();
-		Item i1 = new Item(1);
-		Item i2 = new Item(2);
-		assertEquals(1, i1.getDepth());
-		il.addItem(i1);
-		il.addItem(i2);
-		pv.visitStructure(i1);
-
-		HorizontalRule hr = new HorizontalRule();
-		pv.visitStructure(hr);
-
-
-
-		StyleText st = new StyleText("abc", "br");
-		pv.writeText(st);
-
-		HTMLCode html1 = new HTMLCode("text", "a", "link", "title");
-		HTMLCode html2 = new HTMLCode("text", "img", "link", "title");
-		pv.writeText(html1);
-		pv.writeText(html2);
-
-		Text t = new Text("aaa", "bbb");
-		pv.writeText(t);
-	*/
 	}
 
 
